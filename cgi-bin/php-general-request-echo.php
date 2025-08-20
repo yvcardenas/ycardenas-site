89a4211e4199c4a28616763d198575e5be75e431
yvcardenas <147003383+yvcardenas@users.noreply.github.com>
2025-08-19 17:20:48 -0700

    php-general-request-echo

--- cgi-bin/php-general-request-echo.php
+++ cgi-bin/php-general-request-echo.php
@@ -1,3 +1,24 @@
 <?php
     // Outputs information of whatever it is sent showing method and payload (DELETE, PUT, HEAD, etc)
+    header('Content-Type: text/html; charset=utf-8');
+    $method = $_SERVER['REQUEST_METHOD'];
+    $protocol = $_SERVER['SERVER_PROTOCOL'];
+    $query = $_SERVER['QUERY_STRING'];
+    $body = file_get_contents('php://input');
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">  
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>General Request Echo</title>
+</head>
+<body>
+    <h1>General Request Echo</h1>
+    <hr>
+    <p><strong>Request Method: </strong><?php echo $method; ?></p>
+    <p><strong>Protocol: </strong><?php echo $protocol; ?></p>
+    <p><strong>Query:</strong><?php echo $query; ?></p>
+    <p><strong>Message Body: </strong><?php echo $body; ?></p>
+</body>
+</html>
